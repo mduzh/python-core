afd7e69d14878251796557629fcd562d8ae6ad02
mduzh <duzh.maxim@gmail.com>
2023-06-04 21:20:00 +0300

    var

---
+++ main.py
@@ -0,0 +1,13 @@
+# sep - separates words in a string
+# end - shows end of a string
+# \n - separates strings
+# \... - turns something into a simple symbol
+# \t - makes tabulation
+# min - minimal object in a code
+# max - maximum object in a code
+# abs - finds a module of an object
+# input - get a value from a coder
+
+
+print("Result :", 7, 5)
+num = input("Write your age: ")
---
+++ var-1.lesson.py
@@ -0,0 +1,15 @@
+# del - delete var
+# int , str , float, bool - types of values or vars
+# str - turns other types into str type
+# num = 5
+# word = "Результат"
+# digit = 4.598457
+# boolean = True
+# print(word, digit)
+# print("Результат: ", num)
+
+
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+num1 += 5
+print("Результат: ", num1 + num2)
