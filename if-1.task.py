3f7a52eebaf959a5332f455fc76a9f233b072ba0
mduzh <duzh.maxim@gmail.com>
2023-06-09 21:02:13 +0300

    if 1 done

--- if-1.task.py
+++ if-1.task.py
@@ -1,2 +1,9 @@
 # - Пользователь вводит 2 числа и сохраняет в переменных a и b (не забудь про int())
 # - Если a > b, тогда на экран выводим текст a больше b, иначе выдоим a меньше b
+
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+if a > b:
+    print("Number a is bigger!")
+else:
+    print("Number b is bigger!")
