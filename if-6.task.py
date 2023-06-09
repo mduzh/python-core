db054e9f4b2d186978bef56c2450c04b53345ac2
mduzh <duzh.maxim@gmail.com>
2023-06-09 21:20:53 +0300

    if 6 done

--- if-6.task.py
+++ if-6.task.py
@@ -1,2 +1,8 @@
 # - Пользователь вводит на возраст и сохраняет как число (num функция) в переменной age
 # - Проверить age и вывести на экран, можно ли продавать спиртное.
+
+age = int(input("Enter your age: "))
+if age < 18:
+    print("You will not get an alcohol!")
+else:
+    print("You can get an alcohol!")
