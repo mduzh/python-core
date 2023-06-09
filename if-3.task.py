2d92e4b9fef42617009bba1b6f8680d112781f62
mduzh <duzh.maxim@gmail.com>
2023-06-09 21:15:31 +0300

    if 3 done

--- if-3.task.py
+++ if-3.task.py
@@ -2,3 +2,10 @@
 # - Если a = b и b = c, тогда на экран выводим текст Все числа равны
 # - Иначе выводим 3 числа не совпадают.
 
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+if a == b and b == c:
+    print("All numbers are equal!")
+else:
+    print("They are not equal!")
