de15d8b3542a620647f23d6b77f3e63255ce4775
vduzh <vduzh@tut.by>
2023-06-14 10:31:03 +0300

    2 ошибки

--- for-5.task.py
+++ for-5.task.py
@@ -4,17 +4,18 @@
 # - Вывести на экран числа от 0 до 5. Использовать if в цикле
 
 # # Код ниже
-# for i in range(1, 11):
-#     if i == 6:
-#         break
-#
-#     print(i)
+print("= START =")
+for i in range(1, 11):
+    if i == 6:
+        break
+    print(i)
 
 
 # - Вывести на экран все буквы в строке str до W. Использовать for, а внутри if и break
 # https://youtu.be/vMD6-jzgDvI?t=804 - тут теория
 
 # Код ниже
+print("= START =")
 str = "Hello World!"
 for i in "Hello World":
     if i == "W":
