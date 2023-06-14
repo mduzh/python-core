587696046fc678e918dee503ddda94cf6d199c78
mduzh <duzh.maxim@gmail.com>
2023-06-14 12:03:56 +0300

    for 1 done

--- for-1.task.py
+++ for-1.task.py
@@ -8,10 +8,10 @@ for i in range(1, 11):
 # - Вывести на экран числа от 11 до 20. Использовать for и функцию range
 # Код ниже
 print("= START =")
-for i in range(12, 21):
+for i in range(11, 21):
     print(i)
 # - Вывести на экран числа от 21 до 30 с шагом 2 (21, 23, 25, ..., 29). Использовать for и функцию range
 # Код ниже
 print("= START =")
-for i in range(22, 31, 2):
+for i in range(21, 31, 2):
     print(i)
