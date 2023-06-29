adbaf5b29396810fd0504750dbde369b57d09ce1
vduzh <vduzh@tut.by>
2023-06-29 18:06:35 +0300

    Reviewed

--- list-4.task.py
+++ list-4.task.py
@@ -5,21 +5,23 @@
 # - Используя функцию append добавить в список элементы 0, 1, 2, ..., num -1
 # - Вывести список на экран
 # Code...
-# num = int(input("Enter the number: "))
-# lst = []
-# for i in range(0, num):
-#     lst.append(i)
-# print(lst)
+num = int(input("Enter the number: "))
+lst = []
+for i in range(0, num):
+    lst.append(i)
+print(lst)
+
 # - Пользователь вводит число с экрана и сохраняем значение в переменную num.
 # - Создать список.
 # - Используя функцию append добавить в список элементы 0, 1 * 10, 2 * 10, ..., (num -1) * 10
 # - Вывести список на экран
 # Code...
-# num = int(input("Enter the number: "))
-# lst = []
-# for i in range(0, num):
-#     lst.append(i * 10)
-# print(lst)
+num = int(input("Enter the number: "))
+lst = []
+for i in range(0, num):
+    lst.append(i * 10)
+print(lst)
+
 # - Дан список lst.
 # - Создать 2 новых списка lst1 и lst2
 # - lst1 содержит числа меньше 10
@@ -34,6 +36,7 @@ for i in lst:
     else:
         lst1.append(i)
 print(lst1, lst2)
+
 # - Дан список lst. Сложить первые 3 элемента в списке и добавить результат в lst
 # Подсказка:
 # - используем переменную total
@@ -53,9 +56,12 @@ print(total)
 # - Потом вставить (insert) в начало списка значение total (т.е. [6, 4, 5])
 total = 0
 lst = [1, 2, 3, 4, 5]
+
 for i in range(0, 3):
     total = total + lst[i]
+
 for i in range(0, 3):
     lst.pop(0)
+
 lst.insert(0, total)
 print(lst)
