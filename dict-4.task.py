fe5aca934689d8a2067d224ff47d4b6f0da96700
mduzh <duzh.maxim@gmail.com>
2023-07-04 09:29:56 +0300

    dict 4 done

--- dict-4.task.py
+++ dict-4.task.py
@@ -1,7 +1,8 @@
 # Создать словарь student с ключами name, phone, born, height
 # Вывести на экран словарь student:
 print("= START =")
-
+student = {'name': 'Maxim','phone': '+375333657039', 'born': '20.06.08', 'height': 182}
+print(student)
 
 # Создать словарь student (изначально без ключей) и создать ключи со значениями (использовать []):
 # name - "John"
@@ -11,3 +12,8 @@ print("= START =")
 # Вывести на экран словарь student:
 print("= START =")
 student = {}
+student['name'] = "John"
+student['phone'] = "111-22-33"
+student['born'] = "20-06-08"
+student['height'] = 186
+print(student)
