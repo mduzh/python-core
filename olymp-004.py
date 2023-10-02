01af79e4278d36bb67e19395a247f670643aec6a
maxim <duzh.maxim@gmail.com>
2023-10-02 18:53:43 +0300

    done

--- olymp-004.py
+++ olymp-004.py
@@ -1,2 +1,10 @@
 # В файле data/olymp-004-in.txt заданы числа.
 # Нужно перемножить все числа и результат записать в файл tmp/olymp-004-out.txt
+
+file = open('data/olymp-004-in.txt', 'r')
+res = 1
+for i in file:
+    res = res * int(i)
+    print(res)
+
+file.close()
