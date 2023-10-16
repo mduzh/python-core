405bb8981b9f616b6142a4e6ace761349e381d43
maxim <duzh.maxim@gmail.com>
2023-10-16 20:50:36 +0300

    done

--- olymp-005.py
+++ olymp-005.py
@@ -7,3 +7,23 @@
 # - В переменную res занести значение 'No' - будем сразу считать что нет отрицательного числа.
 # - В циклен проверяем является ли число отрицательным. Если да, тогда в переменную res заносим 'Yes' и ВЫХОДИМ из цикла
 # - потом res записываем в tmp/olymp-005-out.txt
+
+res = 'No'
+file = open('data/olymp-005-in.txt', 'r')
+
+for s in file:
+    num = int(s)
+    print('num:', num)
+    if num < 0:
+        res = 'yes'
+        break
+print(res)
+
+file.close()
+
+
+file1 = open('tmp/olymp-005-out.txt', "w")
+r = str(res)
+file1.write(r)
+
+file1.close()
