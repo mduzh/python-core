f0971fe2cbe81768f15dd39881caf19ffedea387
maxim <duzh.maxim@gmail.com>
2023-10-16 21:04:37 +0300

    done

--- olymp-006.py
+++ olymp-006.py
@@ -9,3 +9,19 @@
 # если нет, то ничего не делаем.
 # - В конце в max_number будет максимальное число - его в tmp/olymp-005-out.txt вывести.
 
+
+max_number = -1
+file = open('data/olymp-006-in.txt', 'r')
+for i in file:
+    num = int(i)
+    if num > max_number:
+        max_number = num
+print('max :', max_number)
+file.close()
+
+
+file1 = open('tmp/olymp-006-out.txt', "w")
+r = str(max_number)
+file1.write(r)
+
+file1.close()
