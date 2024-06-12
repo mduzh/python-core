097c2e84558d6f54b3cc87f1b8fdaa4fe2670844
maxim <duzh.maxim@gmail.com>
2024-06-12 20:06:13 +0300

    done

---
+++ full_course/lesson_003.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+
+print(name)
+
+print(name.capitalize())
+
+
+print(name.upper())
+
+print(dir(name))
--- full_course/lesson_014.py
+++ full_course/lesson_014.py
@@ -1,16 +1,26 @@
 # Вызвать функцию print со строкой Hello World!
-
+print('Hello world!')
 
 # Задать переменную hello_str и ей присвоить строку Hello World!
 # Затем вызвать функцию print с переменной hello_str в качестве параметра
+hello_str = "Hello World!"
+print(hello_str)
 
 
 # Написать свою функцию my_fn с двумя аргументами (они же параметрами) a и b.
 # В теле функции сложить a и b, а значение сохранить в переменной c. Значение с вернуть через return.
 # Вызвать функцию my_fn c параметрами 10 и 20 и значение сохранить в переменной my_fn_result
 # Затем вызвать функцию print с переменной my_fn_result в качестве параметра
+def my_fn(a, b):
+    c = a + b
+    return c
 
+
+my_fn_result = my_fn(10, 20)
+print(my_fn_result)
 
 # Попросить пользователя ввести имя в терминале с помощью функции input.
 # Значение записать в переменную my_name_str
 # Затем вызвать функцию print с переменной my_name_str в качестве параметра
+my_name_str = input("Enter your name: ")
+print(my_name_str)
