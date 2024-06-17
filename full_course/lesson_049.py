b4fa238691e2c89f455e5f8a8cd6e264690773b0
maxim <duzh.maxim@gmail.com>
2024-06-17 18:53:18 +0300

    done

--- full_course/lesson_049.py
+++ full_course/lesson_049.py
@@ -2,12 +2,16 @@
 # С помощью среза (т.е. :) скопировать все элементы списка german_cars в новый список с именем all_cars
 # Распечатать all_cars и убедиться, что вывелось ['Audi', 'BMW', 'Opel', 'Mercedes'].
 german_cars = ['Audi', 'BMW', 'Opel', 'Mercedes']
-
+all_cars = german_cars[:]
+print(all_cars)
 
 # Даны список телефонов phone_lst и пустой список my_phone_lst.
 # С помощью метода списка append скопировать все элементы из списка phone_lst в список my_phone_lst.
 # Распечатать my_phone_lst и убедиться, что вывелось ['IPhone', 'Samsung'].
 phone_lst = ['IPhone', 'Samsung']
 my_phone_lst = []
+my_phone_lst = list()
+my_phone_lst.append(phone_lst)
+print(my_phone_lst)
 
 
