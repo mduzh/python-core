76701165a660281aa0e4b4b0e66af2a22444310d
maxim <duzh.maxim@gmail.com>
2024-07-12 15:23:11 +0300

    done

--- olymp/find_max.py
+++ olymp/find_max.py
@@ -1,5 +1,15 @@
 # Напишите программу, которая запрашивает у пользователя список чисел, разделенных пробелами, и находит
 # наибольшее число в этом списке.
-#
-# Подсказка: см. count_even_odd.py - там есть как разобрать строку.
 
+# Подсказка: см. count_even_odd.py - там есть как разобрать строку.
+values_str = input('Enter the list of numbers: ')
+
+str_values_lst = values_str.split()
+
+max_value = 0
+for str_value in str_values_lst:
+    int_value = int(str_value)
+    if int_value > max_value:
+        max_value = int_value
+
+print(max_value)
