093aadb4529d1bfc9feb21386136ff04795d1b3a
maxim <duzh.maxim@gmail.com>
2024-07-12 20:41:46 +0300

    done

--- olymp/arithmetic/last_digit.py
+++ olymp/arithmetic/last_digit.py
@@ -2,7 +2,12 @@
 #
 # Входные данные
 # Вводится натуральное число.
-
+N = input('Enter your number: ')
+int_list = []
+for value in N:
+    value_int = int(value)
+    int_list.append(value_int)
+print(int_list[-1])
 # Выходные данные
 # Выведите ответ на задачу.
 
