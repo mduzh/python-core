c5bd75633f6327882ebd2794eccd3cb2fc1ad55e
maxim <duzh.maxim@gmail.com>
2024-07-15 21:32:18 +0300

    done

--- olymp/arithmetic/next-odd.py
+++ olymp/arithmetic/next-odd.py
@@ -16,3 +16,10 @@
 # 3
 # выходные данные
 # 5
+
+int_value = int(input('Enter your number: '))
+if int_value % 2 != 0:
+    int_value = int_value + 2
+else:
+    int_value = int_value + 1
+print(int_value)
