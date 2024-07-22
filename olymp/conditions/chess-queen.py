5c93b48965077771c4ab7aadf3171358450b7f1b
mduzh <duzh.maxim@gmail.com>
2024-07-22 09:34:33 +0300

    done

--- olymp/conditions/chess-queen.py
+++ olymp/conditions/chess-queen.py
@@ -24,3 +24,13 @@
 # 3
 # выходные данные
 # NO
+
+x_1 = int(input('Enter the column of the beaten figure: '))
+y_1 = int(input('Enter the line of the beaten figure: '))
+x_2 = int(input('Enter the column of the queen: '))
+y_2 = int(input('Enter the line of the queen: '))
+
+if x_1 == x_2 or y_1 == y_2:
+    print('YES')
+else:
+    print('NO')
