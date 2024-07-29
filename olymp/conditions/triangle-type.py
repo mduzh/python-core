8efa0e6ce21b8fa4fe32dee315f4013dd73316cd
mduzh <duzh.maxim@gmail.com>
2024-07-29 12:05:40 +0300

    done

--- olymp/conditions/triangle-type.py
+++ olymp/conditions/triangle-type.py
@@ -20,3 +20,16 @@
 #
 # выходные данные
 # right
+
+a = int(input('Enter first side: '))
+b = int(input('Enter second side: '))
+c = int(input('Enter third side: '))
+
+if (a * a + b * b == c * c) or (a * a + c * c == b * b) or (c * c + b * b == a * a):
+    print('right')
+elif (a * a + b * b > c * c) or (a * a + c * c > b * b) or (c * c + b * b > a * a):
+    print('acute')
+elif (a * a + b * b < c * c) or (a * a + c * c < b * b) or (c * c + b * b < a * a):
+    print('obtuse')
+else:
+    print('Impossible')
