290308e0e92b4275970027b846ed90621f35355d
mduzh <duzh.maxim@gmail.com>
2024-07-29 11:46:46 +0300

    done

--- olymp/conditions/equals_of_three.py
+++ olymp/conditions/equals_of_three.py
@@ -16,3 +16,14 @@
 # 2
 # выходные данные
 # 2
+
+a = int(input('Enter first number: '))
+b = int(input('Enter second number: '))
+c = int(input('Enter third number: '))
+
+if a == b == c:
+    print(3)
+elif a == b or b == c or a == c:
+    print(2)
+else:
+    print(0)
