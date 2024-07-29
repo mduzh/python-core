910ace3766e3662bc6f370e528be783d91a6904a
mduzh <duzh.maxim@gmail.com>
2024-07-29 11:51:46 +0300

    done

--- olymp/conditions/triangle.py
+++ olymp/conditions/triangle.py
@@ -18,3 +18,12 @@
 # 5
 # выходные данные
 # YES
+
+a = int(input('Enter first side: '))
+b = int(input('Enter second side: '))
+c = int(input('Enter third side: '))
+
+if a + b > c and a + c > b and b + c > a:
+    print('YES')
+else:
+    print('NO')
