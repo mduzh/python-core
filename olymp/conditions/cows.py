cec6c02e51ce11b678486de7062ce0ee6c7a5a09
mduzh <duzh.maxim@gmail.com>
2024-07-30 11:04:57 +0300

    done

--- olymp/conditions/cows.py
+++ olymp/conditions/cows.py
@@ -15,3 +15,21 @@
 # 1
 # выходные данные
 # 1 korova
+
+n = int(input('Enter the amount of cows: '))
+
+# if n == 1 or (n != 11 and n % 10 == 1):
+#     print('На лугу пасется', n, 'корова')
+# elif n in range(2, 5) or n % 10 in range(2, 5):
+#     print('На лугу пасется', n, 'коровы')
+# elif n in range(5, 21) or n % 10 in range(5, 10) or n == 11:
+#     print('На лугу пасется', n, 'коров')
+
+if n != 11 and n % 10 == 1:
+    print('На лугу пасется', n, 'корова')
+elif n % 10 in [2, 3, 4] and n not in [12, 13, 14]:
+    print('На лугу пасется', n, 'коровы')
+else:
+    print('На лугу пасется', n, 'коров')
+
+
