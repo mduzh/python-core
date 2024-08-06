69d9d9cb1abb36aebaa58d2897dba7eb1ed7d05d
mduzh <duzh.maxim@gmail.com>
2024-08-06 12:06:24 +0300

    done

--- olymp/loops/squares.py
+++ olymp/loops/squares.py
@@ -17,9 +17,23 @@
 # 4
 import math
 
+import datetime
+
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
+start = datetime.datetime.now()
 for value in range(a, b + 1):
     if value ** 2 in range(a, b + 1):
         print(value ** 2)
+finish = datetime.datetime.now()
+print(finish - start)
+
+start = datetime.datetime.now()
+for i in range(a, b + 1):
+    x = math.sqrt(i)
+    k = int(x)
+    if k ** 2 == i:
+        print(i)
+finish = datetime.datetime.now()
+print(finish - start)
